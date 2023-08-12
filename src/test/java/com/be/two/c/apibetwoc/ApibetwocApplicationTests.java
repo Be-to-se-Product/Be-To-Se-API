2b6f6b0bbe4d68722f2326979feffa16e8ea06fb
NathanBin <nathan.lopes@sptech.school>
2023-08-11 21:47:39 -0300

    Criação da estrutura de pasta

---
+++ src/test/java/com/be/two/c/apibetwoc/ApibetwocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.two.c.apibetwoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApibetwocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
