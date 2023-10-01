9ad64f3e0614e691874e28236af8367a15b8a1f5
rafasptech <rafael.chuchon@sptech.school>
2023-10-01 20:57:52 -0300

    Alterações essenciais ao padrão observer atribuidas

--- src/main/java/com/be/two/c/apibetwoc/controller/EstabelecimentoController.java
+++ src/main/java/com/be/two/c/apibetwoc/controller/EstabelecimentoController.java
@@ -1,29 +1,63 @@
 package com.be.two.c.apibetwoc.controller;
 
+import com.be.two.c.apibetwoc.dto.CadastroEstabelecimentoDto;
 import com.be.two.c.apibetwoc.dto.CoordenadaDto;
+import com.be.two.c.apibetwoc.model.Estabelecimento;
 import com.be.two.c.apibetwoc.service.EstabelecimentoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/estabelecimentos")
 public class EstabelecimentoController {
 
+
     @Autowired
     private EstabelecimentoService estabelecimentoService;
 
-    @PostMapping("/rota-pessoa")
+    @GetMapping
+    public ResponseEntity<List<Estabelecimento>> listar() {
+        List<Estabelecimento> estabelecimentos = estabelecimentoService.listarTodos();
+        if (estabelecimentos.isEmpty()) {
+            return ResponseEntity.status(204).build();
+        }
+
+        return ResponseEntity.status(200).body(estabelecimentos);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Estabelecimento> cadastrarEstabelecimento(@Valid @RequestBody CadastroEstabelecimentoDto estabelecimento)//, @PathVariable Comerciante comerciante)
+    {
+        //estabelecimento.setComerciante(comerciante);// Em processo de criação; aguardando criação de Controllers para listagem a partir deles, aomente assim para fazer validações.
+        return ResponseEntity.status(201).body(estabelecimentoService.cadastroEstabelecimento(estabelecimento));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Estabelecimento> atualizarEstabelecimento(@Valid @RequestBody CadastroEstabelecimentoDto estabelecimentoDto, @PathVariable Long id) {
+        return estabelecimentoService.atualizar(estabelecimentoDto,id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Estabelecimento> deletarEstabelecimento(@PathVariable Long id){
+        return estabelecimentoService.deletar(id);
+    }
+
+
+    @GetMapping("/rota-pessoa")
     public Long calcularRotaPessoa(@Valid @RequestBody CoordenadaDto coordenadaDto) {
         return estabelecimentoService.calcularRotaPessoa(coordenadaDto);
     }
 
-    @PostMapping("/rota-bicicleta")
+    @GetMapping("/rota-bicicleta")
     public Long calcularRotaBicicleta(@Valid @RequestBody CoordenadaDto coordenadaDto) {
         return estabelecimentoService.calcularRotaBicicleta(coordenadaDto);
     }
 
-    @PostMapping("/rota-carro")
+    @GetMapping("/rota-carro")
     public Long calcularRotaCarro(@Valid @RequestBody CoordenadaDto coordenadaDto) {
         return estabelecimentoService.calcularRotaCarro(coordenadaDto);
     }
--- src/main/java/com/be/two/c/apibetwoc/service/EstabelecimentoService.java
+++ src/main/java/com/be/two/c/apibetwoc/service/EstabelecimentoService.java
@@ -1,16 +1,48 @@
 package com.be.two.c.apibetwoc.service;
 
+import com.be.two.c.apibetwoc.dto.CadastroEstabelecimentoDto;
 import com.be.two.c.apibetwoc.dto.CoordenadaDto;
+import com.be.two.c.apibetwoc.model.Comerciante;
+import com.be.two.c.apibetwoc.model.Estabelecimento;
 import com.be.two.c.apibetwoc.repository.EstabelecimentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EstabelecimentoService {
 
     @Autowired
     private EstabelecimentoRepository estabelecimentoRepository;
+    public Estabelecimento cadastroEstabelecimento(@RequestBody CadastroEstabelecimentoDto estabelecimento){
+        return estabelecimentoRepository.save(new Estabelecimento(null,estabelecimento.getNome(),estabelecimento.getSegmento(),estabelecimento.getDataCriacao(),estabelecimento.getTelefoneContato(),estabelecimento.getEnquadramentoJuridico(),estabelecimento.getReferenciaInstagram(),estabelecimento.getReferenciaFacebook(),estabelecimento.getEmailContato(),estabelecimento.getIsAtivo(),estabelecimento.getComerciante(),estabelecimento.getEndereco()));
+    }
+    public List<Estabelecimento> listarTodos(){
+        List<Estabelecimento> lista = estabelecimentoRepository.findAll();
+        return lista;
+    }
+    public ResponseEntity<Estabelecimento> atualizar(@RequestBody CadastroEstabelecimentoDto estabelecimentoDto, @PathVariable Long id){
+        Estabelecimento cadastravel= new Estabelecimento(id,estabelecimentoDto.getNome(),estabelecimentoDto.getSegmento(),estabelecimentoDto.getDataCriacao(),estabelecimentoDto.getTelefoneContato(),estabelecimentoDto.getEnquadramentoJuridico(),estabelecimentoDto.getReferenciaInstagram(),estabelecimentoDto.getReferenciaFacebook(),estabelecimentoDto.getEmailContato(),estabelecimentoDto.getIsAtivo(),estabelecimentoDto.getComerciante(),estabelecimentoDto.getEndereco());
+        if (this.estabelecimentoRepository.existsById(id)){
+            estabelecimentoRepository.save(cadastravel);
+            return ResponseEntity.ok(cadastravel);
+        };
+
+        return ResponseEntity.notFound().build();
+    }
+    public ResponseEntity<Estabelecimento> deletar(@PathVariable Long id){
+        if (this.estabelecimentoRepository.existsById(id)){
+            estabelecimentoRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 
     public Long calcularRotaPessoa(@RequestBody CoordenadaDto coordenadaDto) {
         Pessoa pessoa = new Pessoa();
