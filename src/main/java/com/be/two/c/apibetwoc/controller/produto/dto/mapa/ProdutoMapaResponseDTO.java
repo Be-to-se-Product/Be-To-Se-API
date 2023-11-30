43dcccd8fa6fbab4a41b4dff5e9d68146e1a8a4d
cesarbmartins <cesar.martins@sptech.school>
2023-11-30 17:49:08 -0300

    adicionando logica de trazer todos os metodos de pagamento do sistema

--- src/main/java/com/be/two/c/apibetwoc/controller/produto/dto/mapa/ProdutoMapaResponseDTO.java
+++ src/main/java/com/be/two/c/apibetwoc/controller/produto/dto/mapa/ProdutoMapaResponseDTO.java
@@ -12,6 +12,8 @@ public class ProdutoMapaResponseDTO {
     private String nome;
     private String categoria;
     private String descricao;
+    private Double precoAtual;
+    private Double precoAntigo;
     private List<String> imagens;
     private List<AvaliacaoMapaResponse> avaliacao;
     private Double mediaAvaliacao;
