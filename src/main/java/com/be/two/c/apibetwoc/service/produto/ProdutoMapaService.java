3631addbc00bfb38b91cc6dd9d9bd1df499e245f
cesarbmartins <cesar.martins@sptech.school>
2024-02-19 10:11:17 -0300

    Arrumando erro da produto Mapa

--- src/main/java/com/be/two/c/apibetwoc/service/produto/ProdutoMapaService.java
+++ src/main/java/com/be/two/c/apibetwoc/service/produto/ProdutoMapaService.java
@@ -8,6 +8,8 @@ import com.be.two.c.apibetwoc.service.imagem.ImagemService;
 import com.be.two.c.apibetwoc.service.produto.specification.ProdutoSpecification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
@@ -23,10 +25,11 @@ public class ProdutoMapaService{
     private final EstabelecimentoRepository estabelecimentoRepository;
     private final ProdutoRepository produtoRepository;
 
-    public List<Produto> retornarProdutos(Double latitude,Double longitude,Double distancia,String produto,String metodoPagamento){
-        List<Integer> estabelecimentos = estabelecimentoRepository.buscarPorLocalizacao(latitude,longitude,distancia);
+    public List<Produto> retornarProdutos(Double latitude, Double longitude, Double distancia, String produto, String metodoPagamento) {
+        List<Integer> estabelecimentos = estabelecimentoRepository.buscarPorLocalizacao(latitude, longitude, distancia);
         Specification<Produto> estabelecimentoSpecification = Specification.where(ProdutoSpecification.filtrarIds(estabelecimentos).and(ProdutoSpecification.name(produto).and(ProdutoSpecification.metodoPagamento(metodoPagamento))));
-        Page<Produto> produtos = produtoRepository.findAll(estabelecimentoSpecification,null);
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Produto> produtos = produtoRepository.findAll(estabelecimentoSpecification, pageable);
         produtos.stream().forEach(produto1 -> produto1.setImagens(produto1.getImagens().stream().map(imagemService::formatterImagensURI).toList()));
         return produtos.stream().toList();
     }
