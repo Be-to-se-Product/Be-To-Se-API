2e7f35f3b0add5a14b7ee60f5f2dcdf0febb75e1
cesarbmartins <cesar.martins@sptech.school>
2024-03-16 16:48:45 -0300

    Logica para enviar todos os produtos quando não enviar localização

--- src/main/java/com/be/two/c/apibetwoc/service/produto/ProdutoMapaService.java
+++ src/main/java/com/be/two/c/apibetwoc/service/produto/ProdutoMapaService.java
@@ -1,6 +1,5 @@
 package com.be.two.c.apibetwoc.service.produto;
 
-import com.be.two.c.apibetwoc.model.Estabelecimento;
 import com.be.two.c.apibetwoc.model.Produto;
 import com.be.two.c.apibetwoc.repository.EstabelecimentoRepository;
 import com.be.two.c.apibetwoc.repository.ProdutoRepository;
@@ -25,8 +24,14 @@ public class ProdutoMapaService{
     public List<Produto> retornarProdutos(Double latitude,Double longitude,Double distancia,String produto,String metodoPagamento){
         List<Integer> estabelecimentos = estabelecimentoRepository.buscarPorLocalizacao(latitude,longitude,distancia);
         Specification<Produto> estabelecimentoSpecification = Specification.where(ProdutoSpecification.filtrarIds(estabelecimentos).and(ProdutoSpecification.name(produto).and(ProdutoSpecification.metodoPagamento(metodoPagamento))));
-        List<Produto> produtos = produtoRepository.findAll(estabelecimentoSpecification);
-        produtos.stream().forEach(produto1 -> produto1.setImagens(produto1.getImagens().stream().map(imagemService::formatterImagensURI).toList()));
+        List<Produto> produtos;
+        if(latitude == null || longitude == null || distancia == null){
+            produtos = produtoRepository.findAll(ProdutoSpecification.name(produto).and(ProdutoSpecification.metodoPagamento(metodoPagamento)));
+        } else {
+            produtos = produtoRepository.findAll(estabelecimentoSpecification);
+        }
+
+        produtos.forEach(produto1 -> produto1.setImagens(produto1.getImagens().stream().map(imagemService::formatterImagensURI).toList()));
         return produtos;
     }
 
