c86ee232683e81e4ef0f62835da0d252c91c0fab
pedrohmunizs <pedro.silva032@sptech.school>
2024-06-08 00:55:41 -0300

    adicionando dto no carrinho

--- src/main/java/com/be/two/c/apibetwoc/controller/carrinho/CarrinhoController.java
+++ src/main/java/com/be/two/c/apibetwoc/controller/carrinho/CarrinhoController.java
@@ -1,6 +1,10 @@
 package com.be.two.c.apibetwoc.controller.carrinho;
 
 import com.be.two.c.apibetwoc.controller.carrinho.dto.CarrinhoRequestDTO;
+import com.be.two.c.apibetwoc.controller.carrinho.dto.CarrinhoResponseDTO;
+import com.be.two.c.apibetwoc.controller.carrinho.mapper.CarrinhoMapper;
+import com.be.two.c.apibetwoc.controller.produto.dto.ProdutoDetalhamentoDto;
+import com.be.two.c.apibetwoc.controller.produto.mapper.ProdutoMapper;
 import com.be.two.c.apibetwoc.model.Carrinho;
 import com.be.two.c.apibetwoc.service.CarrinhoService;
 import jakarta.validation.Valid;
@@ -17,12 +21,14 @@ public class CarrinhoController {
     @Autowired
     private CarrinhoService carrinhoService;
     @GetMapping
-    public ResponseEntity<List<Carrinho>> carrinhoDoConsumidor(@RequestParam Long id){
+    public ResponseEntity<List<CarrinhoResponseDTO>> carrinhoDoConsumidor(@RequestParam Long id){
         List<Carrinho> carrinho = carrinhoService.carrinhoDoConsumidor(id);
         if (carrinho.isEmpty()){
             return ResponseEntity.noContent().build();
         }
-        return ResponseEntity.ok(carrinho);
+        List<CarrinhoResponseDTO> dtos = carrinho.stream().map(CarrinhoMapper::toDto).toList();
+
+        return ResponseEntity.ok(dtos);
     }
     @PostMapping
     public ResponseEntity<Carrinho> adicionandoProduto(@Valid @RequestBody CarrinhoRequestDTO carrinho){
